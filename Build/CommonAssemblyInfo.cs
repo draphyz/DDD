0081137d2021647b6baa8ba091598090ee15e5c8
draphyz <draphyz@gmail.com>
2018-01-04 20:14:28 +0100

    Refactoring

---
+++ Build/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Draphyz")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyVersion("1.0.0.0")] // Used by the runtime to bind to strongly named assemblies, format : MAJOR.0.0.0
+[assembly: AssemblyFileVersion("1.0.0.0")] // Used to uniquely identify a build of the individual assembly, format : MAJOR.MINOR.BUILD.REVISION
+[assembly: AssemblyInformationalVersion("1.0.0")] // Used to represent the product version, format : MAJOR.MINOR.PATCH
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
