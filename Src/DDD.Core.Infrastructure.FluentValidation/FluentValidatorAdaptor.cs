da914cfbf057186043beac84d2d611f1195f0e63
draphyz <draphyz@gmail.com>
2018-01-22 20:08:56 +0100

    Add async method to FluentValidatorAdaptor

--- Src/DDD.Core.Infrastructure.FluentValidation/FluentValidatorAdaptor.cs
+++ Src/DDD.Core.Infrastructure.FluentValidation/FluentValidatorAdaptor.cs
@@ -1,12 +1,14 @@
 ﻿using FluentValidation;
 using FluentValidation.Results;
 using Conditions;
+using System.Threading.Tasks;
 
 namespace DDD.Core.Infrastructure.Validation
 {
     using Mapping;
 
-    public class FluentValidatorAdaptor<T> : Core.Validation.IObjectValidator<T>
+    public class FluentValidatorAdaptor<T> 
+        : Core.Validation.IObjectValidator<T>, Core.Validation.IObjectValidatorAsync<T>
         where T : class
     {
 
@@ -34,6 +36,17 @@ namespace DDD.Core.Infrastructure.Validation
         public Core.Validation.ValidationResult Validate(T obj, string ruleSet = null)
         {
             var result = this.fluentValidator.Validate(obj, ruleSet: ruleSet);
+            return this.resultTranslator.Translate(result);
+        }
+
+        /// <summary>
+        /// Validates asynchronously the specified object.
+        /// </summary>
+        /// <param name="obj">The object to validate.</param>
+        /// <param name="ruleSet">The rule set.</param>
+        public async Task<Core.Validation.ValidationResult> ValidateAsync(T obj, string ruleSet = null)
+        {
+            var result = await this.fluentValidator.ValidateAsync(obj, ruleSet: ruleSet);
             return this.resultTranslator.Translate(result);
         }
 
