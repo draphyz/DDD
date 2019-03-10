00eedcfea18b1be71a63ea622306eee099343eb8
draphyz <draphyz@gmail.com>
2019-03-10 20:42:18 +0100

    Add methods to BelgianSocialSecurityNumber

--- Src/DDD.Common/Domain/BelgianSocialSecurityNumber.cs
+++ Src/DDD.Common/Domain/BelgianSocialSecurityNumber.cs
@@ -1,4 +1,5 @@
 ﻿using Conditions;
+using System;
 
 namespace DDD.Common.Domain
 {
@@ -19,13 +20,146 @@ namespace DDD.Common.Domain
 
         #endregion Constructors
 
+        #region Enums
+
+        public enum Sex
+        {
+            Male,
+            Female
+        }
+
+        #endregion Enums
+
         #region Methods
+
+        /// <summary>
+        /// Computes the check digit based on the 9 first digits.
+        /// </summary>
+        public static int ComputeCheckDigit(string number, bool bornBefore2000 = true)
+        {
+            Condition.Requires(number, nameof(number)).IsLongerOrEqual(9);
+            long identifier;
+            if (bornBefore2000)
+                identifier = long.Parse(number.Substring(0, 9));
+            else
+                identifier = long.Parse($"2{number.Substring(0, 9)}");
+            var modulus = 97;
+            var remainder = modulus - (identifier % modulus);
+            if (remainder == 0) return modulus;
+            return (int)remainder;
+        }
 
         public static BelgianSocialSecurityNumber CreateIfNotEmpty(string number)
         {
             if (string.IsNullOrWhiteSpace(number)) return null;
             return new BelgianSocialSecurityNumber(number);
         }
+
+        /// <summary>
+        /// Returns the birthdate of the person, if known.
+        /// </summary>
+        public DateTime? Birthdate()
+        {
+            var birthMonth = this.BirthMonth();
+            if (birthMonth == null) return null;
+            return new DateTime(this.BirthYear().Value, birthMonth.Value, this.BirthDay().Value);
+        }
+
+        /// <summary>
+        /// Returns the day of birth of the person, if known.
+        /// </summary>
+        public int? BirthDay()
+        {
+            if (this.BirthMonth() == null) return null;
+            return int.Parse(this.Number.Substring(4, 2));
+        }
+
+        /// <summary>
+        /// Returns the month of birth of the person, if known.
+        /// </summary>
+        public int? BirthMonth()
+        {
+            var month = this.MonthNumber();
+            if (month >= 40)
+                month -= 40;
+            else if (month >= 20)
+                month -= 20;
+            if (month == 0) return null;
+            return month;
+        }
+
+        /// <summary>
+        /// Returns the year of birth of the person, if known.
+        /// </summary>
+        public int? BirthYear()
+        {
+            var year = this.Number.Substring(0, 2);
+            if (year == "00") return null;
+            if (this.BornBefore2000())
+                return int.Parse($"19{year}");
+            return int.Parse($"20{year}");
+        }
+        /// <summary>
+        /// Determines whether the person was born before 1st January 2000.
+        /// </summary>
+        public bool BornBefore2000()
+        {
+            var checkDigit = this.CheckDigit();
+            var computedCheckDigit = ComputeCheckDigit(this.Number, bornBefore2000: true);
+            if (computedCheckDigit == checkDigit) return true;
+            computedCheckDigit = ComputeCheckDigit(this.Number, bornBefore2000: false);
+            if (computedCheckDigit == checkDigit) return false;
+            throw new InvalidOperationException("The check digit of the number is invalid.");
+        }
+
+        /// <summary>
+        /// Returns the check digit based on the 9 first digits.
+        /// </summary>
+        public int CheckDigit() => int.Parse(this.Number.Substring(9, 2));
+
+        /// <summary>
+        /// Determines whether the birthdate is only partially known.
+        /// </summary>
+        public bool HasPartialBirthdate() => this.BirthMonth() == null && this.BirthYear() != null;
+
+        /// <summary>
+        /// Determines whether the birthdate is unknown.
+        /// </summary>
+        public bool HasUnknownBirthdate() => this.BirthYear() == null;
+
+        /// <summary>
+        /// Determines whether the social security number is a BIS Register Number (for foreigners).
+        /// </summary>
+        public bool IsBisRegisterNumber() => !this.IsNationalRegisterNumber();
+
+        /// <summary>
+        /// Determines whether the social security number is a National Register Number.
+        /// </summary>
+        public bool IsNationalRegisterNumber() => this.MonthNumber() <= 12;
+
+        /// <summary>
+        /// Returns the unique identifier of the person.
+        /// </summary>
+        public int PersonUniqueIdentifier() => int.Parse(this.Number.Substring(0, 9));
+
+        /// <summary>
+        /// Returns a sequence number used for distinguishing people with identical birthdates.
+        /// </summary>
+        /// <remarks>
+        /// This number is odd for males and even for females.
+        /// </remarks>
+        public int SequenceNumber()
+        {
+            if (this.HasPartialBirthdate()) return int.Parse(this.Number.Substring(5, 4));
+            return int.Parse(this.Number.Substring(6, 3));
+        }
+
+        /// <summary>
+        /// Returns the sex of the person assigned at birth.
+        /// </summary>
+        public Sex SexAtBirth() => this.SequenceNumber() % 2 == 0 ? Sex.Female : Sex.Male;
+
+        private int MonthNumber() => int.Parse(this.Number.Substring(2, 2));
 
         #endregion Methods
 
---
+++ Test/DDD.Common.UnitTests/Domain/BelgianSocialSecurityNumberTests.cs
@@ -0,0 +1,245 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DDD.Common.Domain
+{
+    [Trait("Category", "Unit")]
+    public class BelgianSocialSecurityNumberTests
+    {
+
+        #region Methods
+
+        public static IEnumerable<object[]> ValidNumbersAndExpectedBirthdates()
+        {
+            yield return new object[]
+            {
+                new BelgianSocialSecurityNumber("85073003328"),
+                new DateTime(1985, 7, 30)
+            };
+            yield return new object[]
+            {
+                new BelgianSocialSecurityNumber("53422003890"),
+                new DateTime(1953, 2, 20)
+            };
+            yield return new object[]
+            {
+                new BelgianSocialSecurityNumber("17073003384"),
+                new DateTime(2017, 7, 30)
+            };
+            yield return new object[]
+            {
+                new BelgianSocialSecurityNumber("00000100364"),
+                null
+            };
+            yield return new object[]
+            {
+                new BelgianSocialSecurityNumber("40000095579"),
+                null
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidNumbersAndExpectedBirthdates))]
+        public void Birthdate_WhenValidNumber_ReturnsExpectedValue(BelgianSocialSecurityNumber socialSecurityNumber, DateTime? expectedValue)
+        {
+            // Act
+            var birthdate = socialSecurityNumber.Birthdate();
+            // Assert
+            birthdate.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("85073003328", 30)]
+        [InlineData("53422003890", 20)]
+        [InlineData("00000100364", null)]
+        [InlineData("40000095579", null)]
+        public void BirthDay_WhenValidNumber_ReturnsExpectedValue(string number, int? expectedValue)
+        {
+            // Arrange
+            var socialSecurityNumber = new BelgianSocialSecurityNumber(number);
+            // Act
+            var day = socialSecurityNumber.BirthDay();
+            // Assert
+            day.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("85073003328", 7)]
+        [InlineData("53422003890", 2)]
+        [InlineData("00000100364", null)]
+        [InlineData("40000095579", null)]
+        public void BirthMonth_WhenValidNumber_ReturnsExpectedValue(string number, int? expectedValue)
+        {
+            // Arrange
+            var socialSecurityNumber = new BelgianSocialSecurityNumber(number);
+            // Act
+            var month = socialSecurityNumber.BirthMonth();
+            // Assert
+            month.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("85073003328", 1985)]
+        [InlineData("17073003384", 2017)]
+        [InlineData("53422003890", 1953)]
+        [InlineData("06492803907", 2006)]
+        [InlineData("40000095579", 1940)]
+        [InlineData("00000100364", null)]
+        public void BirthYear_WhenValidNumber_ReturnsExpectedValue(string number, int? expectedValue)
+        {
+            // Arrange
+            var socialSecurityNumber = new BelgianSocialSecurityNumber(number);
+            // Act
+            var year = socialSecurityNumber.BirthYear();
+            // Assert
+            year.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("85073003328", true)]
+        [InlineData("17073003384", false)]
+        [InlineData("53422003890", true)]
+        [InlineData("06492803907", false)]
+        public void BornBefore2000_WhenValidNumber_ReturnsExpectedValue(string number, bool expectedValue)
+        {
+            // Arrange
+            var socialSecurityNumber = new BelgianSocialSecurityNumber(number);
+            // Act
+            var bornBeforeValue = socialSecurityNumber.BornBefore2000();
+            // Assert
+            bornBeforeValue.Should().Be(expectedValue);
+        }
+        [Theory]
+        [InlineData("85073003328", 28)]
+        [InlineData("17073003384", 84)]
+        public void CheckDigit_WhenValidNumber_ReturnsExpectedValue(string number, int expectedValue)
+        {
+            // Arrange
+            var socialSecurityNumber = new BelgianSocialSecurityNumber(number);
+            // Act
+            var checkDigit = socialSecurityNumber.CheckDigit();
+            // Assert
+            checkDigit.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("85073003328", 28, true)]
+        [InlineData("17073003384", 84, false)]
+        [InlineData("53422003890", 90, true)]
+        [InlineData("06492803907", 07, false)]
+        [InlineData("75010112470", 70, true)]
+        [InlineData("750101124", 70, true)]
+        [InlineData("04042400397", 97, false)]
+        [InlineData("40000095579", 79, true)]
+        public void ComputeCheckDigit_WhenValidNumber_ReturnsExpectedValue(string number, int expectedValue, bool bornBefore2000)
+        {
+            // Act
+            var checkDigit = BelgianSocialSecurityNumber.ComputeCheckDigit(number, bornBefore2000);
+            // Assert
+            checkDigit.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("53422003890", false)]
+        [InlineData("00000100364", false)]
+        [InlineData("40000100133", true)]
+        public void HasPartialBirthdate_WhenValidNumber_ReturnsExpectedValue(string number, bool expectedValue)
+        {
+            // Arrange
+            var socialSecurityNumber = new BelgianSocialSecurityNumber(number);
+            // Act
+            var hasPartialBirthdate = socialSecurityNumber.HasPartialBirthdate();
+            // Assert
+            hasPartialBirthdate.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("53422003890", false)]
+        [InlineData("00000100364", true)]
+        [InlineData("40000100133", false)]
+        public void HasUnknownBirthdate_WhenValidNumber_ReturnsExpectedValue(string number, bool expectedValue)
+        {
+            // Arrange
+            var socialSecurityNumber = new BelgianSocialSecurityNumber(number);
+            // Act
+            var hasUnknownBirthdate = socialSecurityNumber.HasUnknownBirthdate();
+            // Assert
+            hasUnknownBirthdate.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("85073003328", false)]
+        [InlineData("17073003384", false)]
+        [InlineData("53422003890", true)]
+        [InlineData("06492803907", true)]
+        public void IsBisRegisterNumber_WhenValidNumber_ReturnsExpectedValue(string number, bool expectedValue)
+        {
+            // Arrange
+            var socialSecurityNumber = new BelgianSocialSecurityNumber(number);
+            // Act
+            var isBisRegisterNumber = socialSecurityNumber.IsBisRegisterNumber();
+            // Assert
+            isBisRegisterNumber.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("85073003328", true)]
+        [InlineData("17073003384", true)]
+        [InlineData("53422003890", false)]
+        [InlineData("06492803907", false)]
+        public void IsNationalRegisterNumber_WhenValidNumber_ReturnsExpectedValue(string number, bool expectedValue)
+        {
+            // Arrange
+            var socialSecurityNumber = new BelgianSocialSecurityNumber(number);
+            // Act
+            var isNationalRegisterNumber = socialSecurityNumber.IsNationalRegisterNumber();
+            // Assert
+            isNationalRegisterNumber.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("85073003328", 850730033)]
+        [InlineData("17073003384", 170730033)]
+        public void PersonUniqueIdentifier_WhenValidNumber_ReturnsExpectedValue(string number, int expectedValue)
+        {
+            // Arrange
+            var socialSecurityNumber = new BelgianSocialSecurityNumber(number);
+            // Act
+            var uniqueIdentifier = socialSecurityNumber.PersonUniqueIdentifier();
+            // Assert
+            uniqueIdentifier.Should().Be(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("85073003328", 33)]
+        [InlineData("53422003890", 38)]
+        [InlineData("00000100364", 3)]
+        [InlineData("40000100133", 1001)]
+        public void SequenceNumber_WhenValidNumber_ReturnsExpectedValue(string number, int expectedValue)
+        {
+            // Arrange
+            var socialSecurityNumber = new BelgianSocialSecurityNumber(number);
+            // Act
+            var sequenceNumber = socialSecurityNumber.SequenceNumber();
+            // Assert
+            sequenceNumber.Should().Be(expectedValue);
+        }
+        [Theory]
+        [InlineData("75010112470", BelgianSocialSecurityNumber.Sex.Female)]
+        [InlineData("85073003328", BelgianSocialSecurityNumber.Sex.Male)]
+        public void SexAtBirth_WhenValidNumber_ReturnsExpectedValue(string number, BelgianSocialSecurityNumber.Sex expectedValue)
+        {
+            // Arrange
+            var socialSecurityNumber = new BelgianSocialSecurityNumber(number);
+            // Act
+            var sexAtBirth = socialSecurityNumber.SexAtBirth();
+            // Assert
+            sexAtBirth.Should().Be(expectedValue);
+        }
+
+        #endregion Methods
+
+    }
+}
