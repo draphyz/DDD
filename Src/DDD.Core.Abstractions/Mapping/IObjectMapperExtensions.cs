d0c6e31f899ba564b06fb069d4163b342c3996b8
draphyz <draphyz@gmail.com>
2019-05-19 21:08:47 +0200

    Add extension methods to mapping classes

---
+++ Src/DDD.Core.Abstractions/Mapping/IObjectMapperExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Conditions;
+using System.Collections.Generic;
+
+namespace DDD.Mapping
+{
+    using Collections;
+
+    public static class IObjectMapperExtensions
+    {
+
+        #region Methods
+
+        public static void Map<TSource, TDestination>(this IObjectMapper<TSource, TDestination> mapper,
+                                                      TSource source,
+                                                      TDestination destination,
+                                                      object options)
+            where TSource : class
+            where TDestination : class
+        {
+            Condition.Requires(mapper, nameof(mapper)).IsNotNull();
+            var dictionary = new Dictionary<string, object>();
+            dictionary.AddObject(options);
+            mapper.Map(source, destination, dictionary);
+        }
+
+        #endregion Methods
+
+    }
+}
---
+++ Src/DDD.Core.Abstractions/Mapping/IObjectTranslatorExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Conditions;
+using System.Collections.Generic;
+
+namespace DDD.Mapping
+{
+    using Collections;
+
+    public static class IObjectTranslatorExtensions
+    {
+
+        #region Methods
+
+        public static TDestination Translate<TSource, TDestination>(this IObjectTranslator<TSource, TDestination> translator,
+                                                                    TSource source,
+                                                                    object options)
+            where TSource : class
+            where TDestination : class
+        {
+            Condition.Requires(translator, nameof(translator)).IsNotNull();
+            var dictionary = new Dictionary<string, object>();
+            dictionary.AddObject(options);
+            return translator.Translate(source, dictionary);
+        }
+
+        public static IEnumerable<TDestination> TranslateCollection<TSource, TDestination>(this IObjectTranslator<TSource, TDestination> translator,
+                                                                                           IEnumerable<TSource> source,
+                                                                                           IDictionary<string, object> options = null)
+            where TSource : class
+            where TDestination : class
+        {
+            Condition.Requires(translator, nameof(translator)).IsNotNull();
+            foreach (var item in source)
+                yield return translator.Translate(item, options);
+        }
+
+        public static IEnumerable<TDestination> TranslateCollection<TSource, TDestination>(this IObjectTranslator<TSource, TDestination> translator,
+                                                                                           IEnumerable<TSource> source,
+                                                                                           object options)
+            where TSource : class
+            where TDestination : class
+        {
+            Condition.Requires(translator, nameof(translator)).IsNotNull();
+            var dictionary = new Dictionary<string, object>();
+            dictionary.AddObject(options);
+            return translator.TranslateCollection(source, dictionary);
+        }
+
+        #endregion Methods
+
+    }
+}
