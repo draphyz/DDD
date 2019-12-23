9f4e7714636700963e639c6983196703c1f8ae9b
draphyz <draphyz@gmail.com>
2019-12-23 19:27:51 +0100

    Rename project

--- Src/DDD.Core.EntityFramework/Properties/AssemblyInfo.cs
+++ Src/DDD.Core.EntityFramework/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("DDD.Core.EF")]
+[assembly: AssemblyTitle("DDD.Core.EntityFramework")]
 [assembly: AssemblyDescription("Implementation of infrastucture components based on Entity Framework.")]
-[assembly: AssemblyProduct("DDD.Core.EF")]
+[assembly: AssemblyProduct("DDD.Core.EntityFramework")]
 [assembly: Guid("6d227aa7-ff90-48ca-b13d-ed23c1fffba5")]
