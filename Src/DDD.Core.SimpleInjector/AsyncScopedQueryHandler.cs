e482ea66cd58efa57e4fcbb0a109de480f8d92d0
draphyz <draphyz@gmail.com>
2019-12-23 19:35:37 +0100

    Add decorators for dependency injection

---
+++ Src/DDD.Core.SimpleInjector/AsyncScopedQueryHandler.cs
@@ -0,0 +1,49 @@
+﻿using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using Conditions;
+using System.Threading.Tasks;
+
+namespace DDD.Core.Infrastructure.DependencyInjection
+{
+    using Application;
+
+    /// <summary>
+    /// A decorator that defines a scope around the asynchronous execution of a query.
+    /// </summary>
+    public class AsyncScopedQueryHandler<TQuery, TResult> : IAsyncQueryHandler<TQuery, TResult>
+        where TQuery : class, IQuery<TResult>
+    {
+
+        #region Fields
+
+        private readonly Container container;
+        private readonly IAsyncQueryHandler<TQuery, TResult> handler;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public AsyncScopedQueryHandler(IAsyncQueryHandler<TQuery, TResult> handler, Container container)
+        {
+            Condition.Requires(handler, nameof(handler)).IsNotNull();
+            Condition.Requires(container, nameof(container)).IsNotNull();
+            this.handler = handler;
+            this.container = container;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public async Task<TResult> HandleAsync(TQuery query)
+        {
+            using (AsyncScopedLifestyle.BeginScope(container))
+            {
+                return await this.handler.HandleAsync(query);
+            }
+        }
+
+        #endregion Methods
+
+    }
+}
---
+++ Src/DDD.Core.SimpleInjector/ThreadScopedCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using Conditions;
+
+namespace DDD.Core.Infrastructure.DependencyInjection
+{
+    using Application;
+
+    /// <summary>
+    /// A decorator that defines a scope around the synchronous execution of a command.
+    /// </summary>
+    public class ThreadScopedCommandHandler<TCommand> : ICommandHandler<TCommand>
+        where TCommand : class, ICommand
+    {
+
+        #region Fields
+
+        private readonly Container container;
+        private readonly ICommandHandler<TCommand> handler;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public ThreadScopedCommandHandler(ICommandHandler<TCommand> handler, Container container)
+        {
+            Condition.Requires(handler, nameof(handler)).IsNotNull();
+            Condition.Requires(container, nameof(container)).IsNotNull();
+            this.handler = handler;
+            this.container = container;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public void Handle(TCommand command)
+        {
+            using (ThreadScopedLifestyle.BeginScope(container))
+            {
+                this.handler.Handle(command);
+            }
+        }
+
+        #endregion Methods
+
+    }
+}
