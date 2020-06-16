fb30e0417230a973d2c7c2c11ce49ae544e4923b
draphyz <draphyz@gmail.com>
2020-06-16 18:37:33 +0200

    Add extensions to SimpleInjector container

---
+++ Src/DDD.Core.SimpleInjector/ContainerExtensions.cs
@@ -0,0 +1,42 @@
+﻿using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Conditions;
+using DDD.DependencyInjection;
+
+namespace DDD.Core.Infrastructure.DependencyInjection
+{
+    public static class ContainerExtensions
+    {
+
+        #region Methods
+
+        public static TService GetNamedInstance<TService>(this Container container, string name) where TService : class
+        {
+            Condition.Requires(container, nameof(container)).IsNotNull();
+            var provider = container.GetInstance<IKeyedServiceProvider<string, TService>>();
+            return provider.GetService(name);
+        }
+
+        public static void RegisterConditional<TService>(this Container container, Func<TService> instanceCreator, Predicate<PredicateContext> predicate)
+                    where TService : class
+        {
+            Condition.Requires(container, nameof(container)).IsNotNull();
+            var registration = Lifestyle.Transient.CreateRegistration(instanceCreator, container);
+            container.RegisterConditional<TService>(registration, predicate);
+        }
+
+        public static void RegisterConditional(this Container container, Type openGenericServiceType, IEnumerable<Assembly> assemblies, Func<Type, bool> predicate)
+        {
+            Condition.Requires(container, nameof(container)).IsNotNull();
+            var implementationTypes = container.GetTypesToRegister(openGenericServiceType, assemblies)
+                                               .Where(predicate);
+            container.Register(openGenericServiceType, implementationTypes);
+        }
+
+        #endregion Methods
+
+    }
+}
---
+++ Src/DDD.Core.SimpleInjector/KeyedServiceProvider.cs
@@ -0,0 +1,59 @@
+﻿using DDD.DependencyInjection;
+using SimpleInjector;
+using Conditions;
+using System.Collections.Generic;
+using System;
+
+namespace DDD.Core.Infrastructure.DependencyInjection
+{
+    public class KeyedServiceProvider<TKey, TService> : IKeyedServiceProvider<TKey, TService>
+        where TService : class
+    {
+
+        #region Fields
+
+        private readonly Container container;
+        private readonly Dictionary<TKey, InstanceProducer<TService>> producers;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public KeyedServiceProvider(Container container, IEqualityComparer<TKey> keyComparer = null)
+        {
+            Condition.Requires(container, nameof(container)).IsNotNull();
+            this.container = container;
+            this.producers = new Dictionary<TKey, InstanceProducer<TService>>(keyComparer);
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public TService GetService(TKey key)
+        {
+            if (this.producers.TryGetValue(key, out var producer))
+                return producer.GetInstance();
+            throw new InvalidOperationException($"No registration for key {key}.");
+        }
+
+        public void Register<TImplementation>(TKey key, Lifestyle lifestyle)
+            where TImplementation : class, TService
+        {
+            Condition.Requires(lifestyle, nameof(lifestyle)).IsNotNull();
+            var producer = lifestyle.CreateProducer<TService, TImplementation>(container);
+            this.producers.Add(key, producer);
+        }
+
+        public void Register(TKey key, Func<TService> instanceCreator, Lifestyle lifestyle)
+        {
+            Condition.Requires(instanceCreator, nameof(instanceCreator)).IsNotNull();
+            Condition.Requires(lifestyle, nameof(lifestyle)).IsNotNull();
+            var producer = lifestyle.CreateProducer(instanceCreator, container);
+            this.producers.Add(key, producer);
+        }
+
+        #endregion Methods
+
+    }
+}
