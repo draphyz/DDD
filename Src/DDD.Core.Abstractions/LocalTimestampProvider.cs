f6328ad520051f16ebe92114f1ab2969f8b9dcb1
draphyz <draphyz@gmail.com>
2022-05-19 16:25:38 +0200

    Implement universal and local timestamp providers

---
+++ Src/DDD.Core.Abstractions/LocalTimestampProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DDD
+{
+    /// <summary>
+    /// Provides timestamps based on the current date and time on this computer, expressed as the local time.
+    /// </summary>
+    public class LocalTimestampProvider : ITimestampProvider
+    {
+        #region Methods
+
+        public DateTime GetTimestamp() => DateTime.Now;
+
+        #endregion Methods
+    }
+}
---
+++ Src/DDD.Core.Abstractions/UniversalTimestampProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DDD
+{
+    /// <summary>
+    /// Provides timestamps based on the current date and time on this computer, expressed as the Coordinated Universal Time (UTC).
+    /// </summary>
+    public class UniversalTimestampProvider : ITimestampProvider
+    {
+
+        #region Methods
+
+        public DateTime GetTimestamp() => DateTime.UtcNow;
+
+        #endregion Methods
+
+    }
+}
