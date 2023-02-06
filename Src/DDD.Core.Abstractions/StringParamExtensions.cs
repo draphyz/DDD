7bfb5ad02b272ce9d8daca57e0e029ea1cc8c6e1
draphyz <draphyz@gmail.com>
2023-02-06 15:26:33 +0100

    Refactoring

--- Src/DDD.Core.Abstractions/StringParamExtensions.cs
+++ Src/DDD.Core.Abstractions/StringParamExtensions.cs
@@ -1,4 +1,5 @@
-﻿using EnsureThat;
+﻿using System;
+using EnsureThat;
 
 namespace DDD
 {
@@ -8,10 +9,10 @@ namespace DDD
         #region Methods
 
         /// <summary>
-        /// Ensures that the string contains only letters.
+        /// Ensures that the string has a minimum length.
         /// </summary>
-        public static void IsAllLetters(this in StringParam param)
-            => Ensure.String.IsAllLetters(param.Value, param.Name, param.OptsFn);
+        public static void HasMinLength(this in StringParam param, int minLength)
+            => Ensure.String.HasMinLength(param.Value, minLength, param.Name, param.OptsFn);
 
         /// <summary>
         /// Ensures that the string contains only digits.
@@ -20,10 +21,16 @@ namespace DDD
             => Ensure.String.IsAllDigits(param.Value, param.Name, param.OptsFn);
 
         /// <summary>
-        /// Ensures that the string has a minimum length.
+        /// Ensures that the string contains only letters.
         /// </summary>
-        public static void HasMinLength(this in StringParam param, int minLength)
-            => Ensure.String.HasMinLength(param.Value, minLength, param.Name, param.OptsFn);
+        public static void IsAllLetters(this in StringParam param)
+            => Ensure.String.IsAllLetters(param.Value, param.Name, param.OptsFn);
+
+        /// <summary>
+        /// Ensures that the string satisfies the specified condition.
+        /// </summary>
+        public static void Satisfy(this in StringParam param, Func<string, bool> predicate)
+            => Ensure.String.Satisfy(param.Value, predicate, param.Name, param.OptsFn);
 
         #endregion Methods
 
