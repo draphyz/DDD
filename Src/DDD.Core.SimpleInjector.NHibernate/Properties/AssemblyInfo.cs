fb9ecaee3898a72923b210fc2fd256f20e941635
draphyz <draphyz@gmail.com>
2023-07-25 12:01:09 +0200

    Add API for dependency injection

--- Src/DDD.Core.SimpleInjector.NHibernate/Properties/AssemblyInfo.cs
+++ Src/DDD.Core.SimpleInjector.NHibernate/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DDD.Core.SimpleInjector.NHibernate")]
-[assembly: AssemblyDescription("Extensions for SimpleInjector to register validation components based on NHibernate.")]
+[assembly: AssemblyDescription("Extensions for SimpleInjector to register database access components based on NHibernate.")]
 [assembly: AssemblyProduct("DDD.Core.SimpleInjector.NHibernate")]
 [assembly: Guid("dd36dcb1-29e3-455d-841e-252fd650ebdf")]
